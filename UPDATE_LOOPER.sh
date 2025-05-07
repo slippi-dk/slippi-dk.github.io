34cbc6ad003e94254e9e368f63c37e7e2635281f
Taza <tazacoaching@gmail.com>
2025-05-07 15:27:40 -0400

    leaderboard init

---
+++ UPDATE_LOOPER.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+rootDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+needsUpdate="$rootDir/needsUpdate.sh"
+update="$rootDir/update.sh"
+
+function doUpdate() {
+	eval $update
+	git add -A
+	git commit -m "data update via codes script"
+	git push
+}
+
+
+cd $rootDir
+
+while :
+do
+	git pull
+	node getCodes.js
+	echo "checking for update"
+	eval $needsUpdate
+	if [ $? -eq 0 ]; then
+		echo "checking for sync"
+		if [[ `git status --porcelain` ]]; then
+			echo "performing sync"
+			git add -A
+			git commit -m "codes update via script"
+			doUpdate
+		fi
+	else
+		echo "performing update"
+		doUpdate
+	fi
+	sleep 1m
+done
---
+++ update.js
@@ -0,0 +1,61 @@
+const { request, gql } = require('graphql-request');
+const fs = require('fs/promises');
+const codes = require('./codes.json');
+const query = gql`
+query AccountManagementPageQuery($cc: String!) {
+  getConnectCode(code: $cc) {
+    user {
+      displayName
+      connectCode {
+        code
+      }
+      rankedNetplayProfile {
+        id
+        ratingOrdinal
+        ratingUpdateCount
+        wins
+        losses
+        dailyGlobalPlacement
+        dailyRegionalPlacement
+        continent
+        characters {
+          character
+          gameCount
+        }
+      }
+    }
+  }
+}
+`;
+const timeout = async ms => new Promise((resolve) => setTimeout(resolve, ms));
+const endpoint = 'https://gql-gateway-2-dot-slippi.uc.r.appspot.com/graphql';
+const getPlayerData = async (cc) => {
+  const variables = { cc: cc.toUpperCase() };
+  let rtCount = 0;
+  while (true) {
+    try {
+      const data = await request(endpoint, query, variables);
+      return data;
+    }
+    catch (e) {
+      console.log(e);
+      rtCount += 1;
+      console.log("Retrying", { cc, rtCount });
+      await timeout(5000);
+    }
+  }
+}
+const getUnixTimestamp = () => Math.floor(Date.now() / 1000);
+const main = async () => {
+  const data = {};
+  for (const [code, tag] of codes) {
+    const playerData = await getPlayerData(code);
+    console.log(code);
+    await timeout(5000);
+    data[code.toUpperCase()] = { ...playerData, tag };
+  }
+  const updatedAt = getUnixTimestamp();
+  const json = { data, updatedAt };
+  await fs.writeFile('./data.json', JSON.stringify(json));
+};
+main()
